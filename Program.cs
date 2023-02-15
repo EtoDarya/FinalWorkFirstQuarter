2c5592b91568a5ce5cc3fe469ac7888e5c69d7ce
Darya <yakimenko-1999@bk.ru>
2023-02-15 21:33:06 +0500

    добавили функции для задавания  параметров и содержания массива, функцию для его вывода

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine ("Введите количество элементов массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] arrString = new string[size];
+
+for(int count = 0; count < size; count++)
+{
+ Console.WriteLine ();
+ Console.WriteLine ("Введите элемент массива: ");
+ arrString[count] = Console.ReadLine();
+}
+    
+void ShowArray(string [] array) 
+{     
+ for (int i = 0; i < array.Length; i++)     
+    {         
+       Console.Write(array[i] + "; ");     
+    }     
+ Console.WriteLine(); 
+}
+    
+Console.WriteLine();
+ShowArray(arrString);
